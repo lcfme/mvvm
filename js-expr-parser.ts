8d98133a14e957107c18e2a4645b42a782e53a00
lcfme <lcfme@live.com>
2019-04-04 15:04:02 +0800

    work on lexer parser evaluator. all these are funking hard.

---
+++ js-expr-parser.ts
@@ -0,0 +1,60 @@
+interface CompilerPrototype<R, P, N> {
+  read(): R;
+  peek(): P;
+  next(): N;
+}
+
+interface Token<T> {
+  Type: string;
+  Value: T;
+}
+
+function Lexer(input: any): CompilerPrototype<string, string, Token<any>> {
+  if (typeof input !== "string") {
+    throw new Error(
+      "invalid type of input, expected string but got: " + typeof input
+    );
+  }
+
+  function skipWhiteSpace(l: Lexer) {
+    let ch = l.peek();
+    while (ch && /\s/.test(ch)) {
+      ch = l.read();
+    }
+  }
+
+  class Lexer implements Lexer {
+    at: number = 0;
+    input: string = "";
+    ch: string = "";
+    constructor(input: string) {
+      this.input = input;
+    }
+    read(): string {
+      const ch = (this.ch = this.input.charAt(this.at));
+      if (ch === "") {
+        return ch;
+      }
+      this.at++;
+      return ch;
+    }
+    peek(): string {
+      return this.ch || this.read();
+    }
+    next(): Token<any> {
+      skipWhiteSpace(this);
+      let ch = this.peek();
+      if (ch === "") {
+        return null;
+      }
+      switch (ch) {
+        case "":
+      }
+      return {
+        Type: "",
+        Value: undefined
+      };
+    }
+  }
+  return new Lexer(input);
+}
